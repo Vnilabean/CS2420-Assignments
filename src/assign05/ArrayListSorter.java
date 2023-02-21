ee5fd6a851c60ddf1ae5804e7fa17029dbbc670e
Phil <95306018+Vnilabean@users.noreply.github.com>
2023-02-20 23:12:20 -0700

    final

--- src/assign05/ArrayListSorter.java
+++ src/assign05/ArrayListSorter.java
@@ -1,12 +1,15 @@
 package assign05;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 
 
 public class ArrayListSorter {
     private static final int insertThresh = 3;
+    //1: returns middle index
+    //2: returns first index
+    //3: returns greater item of index 0 and index size-1
+    private static final int pivotType = 1;
 
     // mergesort ------------------------------------------------------------------------------------------
 
@@ -15,50 +18,57 @@ public class ArrayListSorter {
      * It will use the Comparable interface's compareTo method to determine the correct ordering of elements.
      * Your implementation must switch over to insertion sort when the size of the sublist to be sorted meets
      * a certain threshold (i.e., becomes small enough).
+     *
      * @param arr array to be merged sorted
      * @param <T> generic type
      */
     public static <T extends Comparable<? super T>> void mergesort(ArrayList<T> arr) {
-        int arrSize= arr.size();
-          ArrayList<T> tempMergeArray = new ArrayList<>(arrSize);
-         for (int i =0;i< arr.size();i++) {
-             tempMergeArray.add(null);
-         }
-        mergeSort(arr,0,arr.size()-1, tempMergeArray);
+        int arrSize = arr.size();
+
+        // initializes temp array to arr size to use set to make merging quick
+        ArrayList<T> tempMergeArray = new ArrayList<>(arrSize);
+        for (int i = 0; i < arr.size(); i++) {
+            tempMergeArray.add(null);
+        }
+        mergeSort(arr, 0, arr.size() - 1, tempMergeArray);
         arr = tempMergeArray;
     }
 
     /**
      * recursive mergesort method that only takes input from the driver method.
-     * @param arr given array to sort
+     *
+     * @param arr   given array to sort
      * @param start start index
-     * @param end ending index
-     * @param <T> generic type
+     * @param end   ending index
+     * @param <T>   generic type
      */
-    private static <T extends Comparable<? super T>> void mergeSort(ArrayList<T> arr, int start, int end,ArrayList<T> tempMergeArray){
+    private static <T extends Comparable<? super T>> void mergeSort(ArrayList<T> arr, int start, int end, ArrayList<T> tempMergeArray) {
+        // uses private final item to change insertion sort threshold
         if (start + insertThresh > end) {
             insertionSort(arr);
             return;
         }
-        int mid = start + (end - start)/2;
-        mergeSort(arr,start,mid,tempMergeArray);
-        mergeSort(arr,mid + 1,end,tempMergeArray);
-        merge(arr, start, mid, end,tempMergeArray);
+        int mid = start + (end - start) / 2;
+        mergeSort(arr, start, mid, tempMergeArray);
+        mergeSort(arr, mid + 1, end, tempMergeArray);
+        merge(arr, start, mid, end, tempMergeArray);
     }
 
     /**
      * final step in merge sort of merging the sorted partitioned sections of the array.
-     * @param arr given array
-     * @param start starting index
+     *
+     * @param arr    given array
+     * @param start  starting index
      * @param middle middle index
-     * @param end ending index
-     * @param <T> generic type
+     * @param end    ending index
+     * @param <T>    generic type
      */
-    private static <T extends Comparable<? super T>> void merge(ArrayList<T> arr, int start, int middle, int end, ArrayList<T> tempMergeArray){
+    protected static <T extends Comparable<? super T>> void merge(ArrayList<T> arr, int start, int middle, int end, ArrayList<T> tempMergeArray) {
         int leftPointer = start;
         int rightPointer = middle;
         int insertionPosition = start;
 
+        // move pointer and compares to other pointer and adds the smaller to temp array
         while (leftPointer < middle && rightPointer <= end) {
             if (arr.get(leftPointer).compareTo(arr.get(rightPointer)) <= 0) {
                 tempMergeArray.set(insertionPosition, arr.get(leftPointer));
@@ -70,70 +80,36 @@ public class ArrayListSorter {
                 rightPointer++;
             }
         }
-            while (leftPointer < middle) {
-                tempMergeArray.set(insertionPosition, arr.get(leftPointer));
-                insertionPosition++;
-                leftPointer++;
-            }
-            while (rightPointer <= end) {
-                tempMergeArray.set(insertionPosition, arr.get(rightPointer));
-                insertionPosition++;
-                rightPointer++;
 
-
-
-
-//        var left = middle - start + 1;
-//        var right = end - middle;
-//        var leftArr = new ArrayList<T>(left);
-//        var rightArr = new ArrayList<T>(right);
-//        for (int i = 0; i < left; i++)
-//            leftArr.add(arr.get(i));
-//        for (int i = middle + 1; i < right; i++)
-//            rightArr.add(arr.get(i));
-//        int leftInd = 0;
-//        int rightInd = 0;
-//        int mergedInd = start;
-//        while(leftInd < left && rightInd < right){
-//            if(leftArr.get(leftInd).compareTo(rightArr.get(rightInd)) < 0){
-//                arr.set(mergedInd, leftArr.get(leftInd));
-//                leftInd++;
-//            }
-//            else{
-//                arr.set(mergedInd, rightArr.get(rightInd));
-//                rightInd++;
-//            }
-//            mergedInd++;
-//        }
-//        for (int i = 0; i < left; i++){
-//            arr.set(mergedInd, leftArr.get(leftInd));
-//            leftInd++;
-//            mergedInd++;
-//        }
-//        for (int i = 0; i < right; i++){
-//            arr.set(mergedInd, rightArr.get(rightInd));
-//            rightInd++;
-//            mergedInd++;
-//        }
+        // adds left over items if they were not added from the last while loop
+        while (leftPointer < middle) {
+            tempMergeArray.set(insertionPosition, arr.get(leftPointer));
+            insertionPosition++;
+            leftPointer++;
+        }
+        while (rightPointer <= end) {
+            tempMergeArray.set(insertionPosition, arr.get(rightPointer));
+            insertionPosition++;
+            rightPointer++;
         }
     }
+
     /**
      * Insertion sort for mergeSort threshold
+     *
      * @param arr array to be insertion sorted
      * @param <T> Generic
      */
     private static <T extends Comparable<? super T>> void insertionSort(ArrayList<T> arr) {
         int size = arr.size();
-        int i = 1;
-        while (i < size) {
+        for (int i = 1; i < size; i++) {
             T current = arr.get(i);
-            int prev = i-1;
+            int prev = i - 1;
             while (prev >= 0 && arr.get(prev).compareTo(current) > 0) {
-                arr.set(i, arr.get(prev));
+                arr.set(prev + 1, arr.get(prev));
                 prev = prev - 1;
             }
             arr.set(prev + 1, current);
-            i++;
         }
     }
 
@@ -143,26 +119,28 @@ public class ArrayListSorter {
      * This method performs a quicksort on the generic ArrayList given as input.
      * You must implement three different strategies for determining the pivot, and your
      * implementation should be able to easily switch among these strategies.
+     *
      * @param arr to be sorted
      * @param <T> the type of array
      */
     public static <T extends Comparable<? super T>> void quicksort(ArrayList<T> arr) {
-        quicksort(arr,0,arr.size()-1);
+        quicksort(arr, 0, arr.size() - 1);
     }
 
     /**
      * compares the start and end and if the start is lower than the end this calles partition
      * on the arr and its starting and ending position then recursively calls quickSort on the arrays that
      * are parted from start to partition and partition to end
-     * @param arr to be sorted
+     *
+     * @param arr   to be sorted
      * @param start position
-     * @param end position
-     * @param <T> type to be used
+     * @param end   position
+     * @param <T>   type to be used
      */
     private static <T extends Comparable<? super T>> void quicksort(ArrayList<T> arr, int start, int end) {
-        if (start < end){
+        if (start < end) {
             int part = quickSortPartition(arr, start, end);
-            quicksort(arr, start, part-1);
+            quicksort(arr, start, part - 1);
             quicksort(arr, part, end);
         }
     }
@@ -171,15 +149,16 @@ public class ArrayListSorter {
      * Partitions an array. First selects a pivot and the left and right side of the array. compares the left and right
      * side of the array to the pivot and places all smaller (smaller than pivot) to left of pivot and
      * all greater elements to right of the pivot
-     * @param arr
-     * @param start
-     * @param end
+     *
+     * @param arr array
+     * @param start starting index for recursion
+     * @param end ending index for recursion
+     * @param <T> generic type
      * @return
-     * @param <T>
      */
-    private static <T extends Comparable<? super T>> int quickSortPartition(ArrayList<T> arr, int start, int end) {
+    protected static <T extends Comparable<? super T>> int quickSortPartition(ArrayList<T> arr, int start, int end) {
 
-        int pivotPosition = pivot(arr,1,start,end );
+        int pivotPosition = pivot(arr, pivotType, start, end);
         swap(arr, pivotPosition, end);
         // pivot point
         T pivotItem = arr.get(end);
@@ -189,44 +168,51 @@ public class ArrayListSorter {
         int right = end;
         //while the start is lesser than the end index
         while (left <= right) {
-            while(left < end && arr.get(left).compareTo(pivotItem) < 0) {
+            while (left < end && arr.get(left).compareTo(pivotItem) < 0) {
                 left++;
             }
             while (right >= start && arr.get(right).compareTo(pivotItem) > 0) {
                 right--;
             }
-            if(left<=right) {
+            if (left <= right) {
                 swap(arr, left, right);
                 left++;
                 right--;
             }
 
-            }
+        }
         return left;
     }
 
-
-    private static <T> void swap(ArrayList<T> arr,int index1,int index2) {
+    /**
+     * small method to swap two items in a array
+     * @param arr array
+     * @param index1 index of first item to swap
+     * @param index2 index of second item to swap
+     * @param <T>
+     */
+    private static <T> void swap(ArrayList<T> arr, int index1, int index2) {
         T temp = arr.get(index1);
-        arr.set(index1,arr.get(index2));
-        arr.set(index2,temp);
+        arr.set(index1, arr.get(index2));
+        arr.set(index2, temp);
     }
 
     /**
      * pivot method to return int position for best pivot.
-     * @param arr
+     *
+     * @param arr array
      * @param type Allows changing the pivot strategy by setting type to 1-3
      *             1: returns middle index
      *             2: returns first index
      *             3: returns greater item of index 0 and index size-1
      *             Default: the last index
+     * @param <T>  generic type
      * @return int position on the given array for the pivot
-     * @param <T> generic type
      */
     private static <T extends Comparable<? super T>> int pivot(ArrayList<T> arr, int type, int start, int end) {
         switch (type) {
             case 1 -> {
-                return ((end-start) / 2) + start;
+                return ((end - start) / 2) + start;
             }
             case 2 -> {
                 return start;
@@ -239,7 +225,9 @@ public class ArrayListSorter {
                     return end;
                 }
             }
-            default -> { return end; }
+            default -> {
+                return end;
+            }
         }
     }
 
@@ -247,12 +235,13 @@ public class ArrayListSorter {
 
     /**
      * This method generates and returns an ArrayList of integers 1 to size in ascending order.
+     *
      * @param size
      * @return
      */
     public static ArrayList<Integer> generateAscending(int size) {
         ArrayList<Integer> temp = new ArrayList<Integer>(size);
-        for (int i = 1;i<=size;i++) {
+        for (int i = 1; i <= size; i++) {
             temp.add(i);
         }
         return temp;
@@ -261,12 +250,13 @@ public class ArrayListSorter {
     /**
      * This method generates and returns an ArrayList of integers 1 to size in permuted order (i,e., randomly ordered).
      * You may adapt the shuffle method written in Class Meeting 10 or make use of Java's Collections.shuffle method.
+     *
      * @param size
      * @return random array
      */
     public static ArrayList<Integer> generatePermuted(int size) {
         ArrayList<Integer> temp = new ArrayList<Integer>(size);
-        for (int i = 1;i<=size;i++) {
+        for (int i = 1; i <= size; i++) {
             temp.add(i);
         }
         Collections.shuffle(temp);
@@ -275,12 +265,13 @@ public class ArrayListSorter {
 
     /**
      * This method generates and returns an ArrayList of integers 1 to size in descending order.
+     *
      * @param size
      * @return
      */
     public static ArrayList<Integer> generateDescending(int size) {
         ArrayList<Integer> temp = new ArrayList<Integer>(size);
-        for (int i = size;i>0;i--) {
+        for (int i = size; i > 0; i--) {
             temp.add(i);
         }
         return temp;
--- src/assign05/ArrayListSorterTest.java
+++ src/assign05/ArrayListSorterTest.java
@@ -2,8 +2,10 @@ package assign05;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 class ArrayListSorterTest {
     ArrayList<Integer> permutedArr, ascendingArr, descendingArr;
@@ -21,15 +23,81 @@ class ArrayListSorterTest {
         ArrayListSorter.mergesort(permutedArr);
         System.out.println(permutedArr.toString());
     }
+    @Test
+    void mergesortSmall() {
+        ArrayList<Integer> smallArr = new ArrayList<>();
+        ArrayList<Integer> smallArrSorted = new ArrayList<>();
+        smallArrSorted.add(1);
+        smallArrSorted.add(2);
+        smallArr.add(2);
+        smallArr.add(1);
+        ArrayListSorter.mergesort(smallArr);
+        assertEquals(smallArr,smallArrSorted);
+    }
 
     @Test
-    void quicksort() {
-        System.out.println(permutedArr.toString());
-        ArrayListSorter.quicksort(permutedArr);
-        System.out.println(permutedArr.toString());
-        ArrayListSorter.quicksort(descendingArr);
-        System.out.println(descendingArr.toString());
+    void mergesortEmpty() {
+        ArrayList<Integer> smallArr = new ArrayList<>();
+        ArrayListSorter.mergesort(smallArr);
+        assertEquals(smallArr,smallArr);
     }
+
+    @Test
+    void mergesortVeryLarge() {
+        ArrayList<Integer> largeArrSorted = ArrayListSorter.generateAscending(200);
+        ArrayList<Integer> largeArrPerm = ArrayListSorter.generatePermuted(200);
+        ArrayListSorter.mergesort(largeArrPerm);
+        assertEquals(largeArrSorted,largeArrPerm);
+    }
+
+    @Test
+    void mergesortDescending() {
+        ArrayListSorter.mergesort(descendingArr);
+        assertEquals(descendingArr,ascendingArr);
+    }
+
+    //quicksortInteger tests---------------------------------------------------
+
+    @Test
+    void quicksortPermuted() {
+        ArrayListSorter.quicksort(permutedArr);
+        assertEquals(permutedArr,ascendingArr);
+    }
+
+    @Test
+    void quicksortSmall() {
+        ArrayList<Integer> smallArr = new ArrayList<>();
+        ArrayList<Integer> smallArrSorted = new ArrayList<>();
+        smallArrSorted.add(1);
+        smallArrSorted.add(2);
+        smallArr.add(2);
+        smallArr.add(1);
+        ArrayListSorter.quicksort(smallArr);
+        assertEquals(smallArr,smallArrSorted);
+    }
+
+    @Test
+    void quicksortEmpty() {
+        ArrayList<Integer> smallArr = new ArrayList<>();
+        ArrayListSorter.quicksort(smallArr);
+        assertEquals(smallArr,smallArr);
+    }
+
+    @Test
+    void quicksortVeryLarge() {
+        ArrayList<Integer> largeArrSorted = ArrayListSorter.generateAscending(200);
+        ArrayList<Integer> largeArrPerm = ArrayListSorter.generatePermuted(200);
+        ArrayListSorter.quicksort(largeArrPerm);
+        assertEquals(largeArrSorted,largeArrPerm);
+    }
+
+    @Test
+    void quickSortDescending() {
+        ArrayListSorter.quicksort(descendingArr);
+        assertEquals(descendingArr,ascendingArr);
+    }
+
+    //generate array tests----------------------------------------
 
     @Test
     void generateAscending() {
@@ -42,4 +110,55 @@ class ArrayListSorterTest {
     @Test
     void generateDescending() {
     }
+
+    //more complex private method tests---------------------------------------------------
+
+    @Test
+    void testMerge() {
+        ArrayList<Integer> temp = new ArrayList<>();
+        for (int i = 0;i<descendingArr.size();i++) {
+            temp.add(null);
+        }
+        ArrayListSorter.merge(descendingArr,0,descendingArr.size()/2,descendingArr.size()-1,temp);
+        // essential what this did was sort as if it was two singular halves of values as merge should keep doing this until very small.
+        assertEquals(Arrays.toString(temp.toArray()),"[10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11]");
+    }
+
+    @Test
+    void testPartition(){
+
+        ArrayListSorter.quickSortPartition(descendingArr,0,descendingArr.size()-1);
+        // partition moves items around the pivot select which is 11 in this case, which shows this works
+        assertEquals(Arrays.toString(descendingArr.toArray()), "[11, 2, 3, 4, 5, 6, 7, 8, 9, 1, 10, 12, 13, 14, 15, 16, 17, 18, 19, 20]");
+    }
+
+    @Test
+    void testInsertionSort(){
+        ArrayListSorter.insertionSort(permutedArr);
+        assertEquals(permutedArr, ascendingArr);
+    }
+
+    //mergesortString tests-----------------------------------------------------------------
+
+    @Test
+    void mergeSortString(){
+        ArrayListSorter.mergesort(names);
+        assertEquals(names.get(0), "Abba");
+        assertEquals(names.get(1), "Bernadet");
+        assertEquals(names.get(2), "John");
+        assertEquals(names.get(3), "Kyle");
+        assertEquals(names.get(4), "Zaja");
+    }
+
+    @Test
+    void quickSortString(){
+        ArrayListSorter.quicksort(names);
+        assertEquals(names.get(0), "Abba");
+        assertEquals(names.get(1), "Bernadet");
+        assertEquals(names.get(2), "John");
+        assertEquals(names.get(3), "Kyle");
+        assertEquals(names.get(4), "Zaja");
+
+    }
+
 }
